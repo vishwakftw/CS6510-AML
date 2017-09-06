1786b8d6abda300de6625b1bfdafd8ce3a7808a2
vishwakftw <cs15btech11043@iith.ac.in>
2017-09-06 16:40:18 +0530

    PA-1 Update 11

--- Programming-Assignment-1/Question4/dataloader.py
+++ Programming-Assignment-1/Question4/dataloader.py
@@ -2,19 +2,78 @@ import os
 import numpy as np
 
 def make_dict(cur_dict, key, values):
+	"""
+		Function to make a dictionary for the textual input
+		Args:
+			cur_dict	= Pre-existing dictionary object
+			key		= Name of the key
+			values		= Values to be taken within that key
+		Returns:
+			dictionary object
+	"""
 	cur_dict[key] = {}
 	for i, v in enumerate(values):
 		cur_dict[key][v] = i
 	return cur_dict
 	
-def make_dataset(root):
-	cur_path	= os.path.join(root, 'data')
-	
-	train_path	= os.path.join(cur_path, 'train.csv')
-	tr	= np.genfromtxt(train_path, delimiter=', ', dtype=str)
+def get_dataset(root):
+	"""
+		Function to make the dataset
+		Args:
+			root		= Root destination for the data folder
+		Returns:
+			2-tuple of numpy.ndarray of shape (n_points, n_features (+1)) (+1 for training - outputs are appended to each input)
+			1st element is the training data, 2nd element is the testing data
+	"""
+	# Construct dict based on README, and same encoding for text will be used for train and test data
+	# Just data preprocessing section
 	cur_dict	= {}	
 	ref_dict	= {'workclass': 1, 'education': 3, 'marital-status': 5, 'occupation': 6, 'relationship': 7, 'race': 8, 'sex': 9, 'native-country': 13}
-	for key in ref_dict:
-		cur_dict	= make_dict(cur_dict, key, list(set(tr[:,ref_dict[key]].tolist())))
+	val_dict	= {
+	
+			   'workclass'		: [' Private', ' Self-emp-not-inc', ' Self-emp-inc', ' Federal-gov', ' Local-gov', ' State-gov', ' Without-pay', ' Never-worked', ' ?'],
+
+			   'education'		: [' Bachelors', ' Some-college', ' 11th', ' HS-grad', ' Prof-school', ' Assoc-acdm', ' Assoc-voc', ' 9th', ' 7th-8th', ' 12th', ' Masters', ' 1st-4th', ' 10th', ' Doctorate', ' 5th-6th', ' Preschool', ' ?'],
+			   
+			   'marital-status'	: [' Married-civ-spouse', ' Divorced', ' Never-married', ' Separated', ' Widowed', ' Married-spouse-absent', ' Married-AF-spouse', ' ?'],
+			   
+			   'occupation'		: [' Tech-support', ' Craft-repair', ' Other-service', ' Sales', ' Exec-managerial', ' Prof-specialty', ' Handlers-cleaners', ' Machine-op-inspct', ' Adm-clerical', ' Farming-fishing', ' Transport-moving', ' Priv-house-serv', ' Protective-serv', ' Armed-Forces', ' ?'],
+
+			   'relationship'	: [' Wife', ' Own-child', ' Husband', ' Not-in-family', ' Other-relative', ' Unmarried', ' ?'],
+
+			   'race'		: [' White', ' Asian-Pac-Islander', ' Amer-Indian-Eskimo', ' Other', ' Black', ' ?'],
+
+			   'sex'		: [' Female', ' Male', ' ?'],
+
+			   'native-country'	: [' United-States', ' Cambodia', ' England', ' Puerto-Rico', ' Canada', ' Germany', ' Outlying-US(Guam-USVI-etc)', ' India', ' Japan', ' Greece', ' South', ' China', ' Cuba', ' Iran', ' Honduras', ' Philippines', ' Italy', ' Poland', ' Jamaica', ' Vietnam', ' Mexico', ' Portugal', ' Ireland', ' France', ' Dominican-Republic', ' Laos', ' Ecuador', ' Taiwan', ' Haiti', ' Columbia', ' Hungary', ' Guatemala', ' Nicaragua', ' Scotland', ' Thailand', ' Yugoslavia', ' El-Salvador', ' Trinadad&Tobago', ' Peru', ' Hong', ' Holand-Netherlands', ' ?']
+			  }
+
+	for key in ref_dict.keys():
+		cur_dict	= make_dict(cur_dict, key, val_dict[key])
 		
-	return cur_dict
+	cur_path	= os.path.join(root, 'data')
+	
+	# Get train dataset file
+	train_path	= os.path.join(cur_path, 'train.csv')
+	train_data	= np.genfromtxt(train_path, delimiter=',', dtype=str)
+
+	# Replace words with values in training data
+	for i in range(0, train_data.shape[0]):
+		for key in ref_dict.keys():
+			train_data[i, ref_dict[key]] 	= cur_dict[key][train_data[i, ref_dict[key]]]
+	
+	# Get test dataset file
+	test_path	= os.path.join(cur_path, 'test.csv')
+	test_data	= np.genfromtxt(test_path, delimiter=',', dtype=str, usecols=range(0, 14))
+	
+	# Replace words with values in testing data
+	for i in range(0, test_data.shape[0]):
+		for key in ref_dict.keys():
+			test_data[i, ref_dict[key]]	= cur_dict[key][test_data[i, ref_dict[key]]]
+			
+	train_data	= train_data.astype(float)
+	test_data	= test_data.astype(float)
+	assert np.isnan(train_data).any() == False, "Some conversions in Training Data have been missed"
+	assert np.isnan(train_data).any() == False, "Some conversions in Testing Data have been missed"
+	
+	return train_data, test_data
---
+++ Programming-Assignment-1/Question4/single_kernel.py
@@ -0,0 +1,53 @@
+import numpy as np
+from sklearn import svm
+from dataloader import get_dataset
+from argparse import ArgumentParser
+from kernels import linear_gram_matrix as lgm
+from kernels import gaussian_gram_matrix as ggm
+from kernels import polynomial_gram_matrix as pgm
+from sklearn.model_selection import StratifiedKFold
+
+parser	= ArgumentParser()
+parser.add_argument('--root', required=True, help='root folder for the dataset')
+parser.add_argument('--kernel', required=True, help='kernel : linear | polynomial | gaussian')
+parser.add_argument('--q', type=int, default=2, help='parameter for polynomial kernel')
+parser.add_argument('--sigma', type=float, default=1, help='parameter for gaussian kernel')
+opt	= parser.parse_args()
+	
+train, test_x		= get_dataset(root=sys.argv[1])
+train_x, train_y	= train[ : , 0 : train.shape[1] - 1], train[ : , train.shape[1] - 1 ]
+
+svm_classifier	= svm.SVC(kernel='precomputed')
+cross_valid	= StratifiedKFold(n_splits=5)
+
+tr_accuracies	= []
+va_accuracies	= []
+
+for tr, va in cross_valid.split(train_x, train_y):
+	
+	print('Started cross validation split: {0}'.format(len(tr_accuracies) + 1))	
+	tr_x, va_x	= train_x[tr], train_x[va]
+	tr_y, va_y	= train_y[tr], train_y[va]
+
+	if opt.kernel == 'linear':
+		tr_gram_matrix	= lgm(tr_x, tr_x)
+		va_gram_matrix	= lgm(va_x, tr_x)
+	elif opt.kernel == 'polynomial':
+		tr_gram_matrix	= pgm(tr_x, tr_x, opt.q)
+		va_gram_matrix	= pgm(va_x, tr_x, opt.q)
+	elif opt.kernel	== 'gaussian':
+		tr_gram_matrix	= ggm(tr_x, tr_x, opt.sigma)
+		va_gram_matrix	= ggm(va_x, tr_x, opt.sigma)
+
+	svm_classifier.fit(tr_gram_matrix, tr_y)
+	tr_predictions	= svm_classifier.predict(tr_gram_matrix)
+	va_predictions	= svm_classifier.predict(va_gram_matrix)
+
+	tr_accuracy	= 1 - np.abs(tr_predictions - tr_y).sum()/tr_y.shape[0]
+	va_accuracy	= 1 - np.abs(va_predictions - va_y).sum()/va_y.shape[0]
+	
+	tr_accuracies.append(tr_accuracy)
+	va_accuracies.append(va_accuracy)
+	
+print('Train Accuracies across all folds: {0}'.format(tr_accuracies))
+print('Validation Accuracies across all folds: {0}'.format(va_accuracies))
