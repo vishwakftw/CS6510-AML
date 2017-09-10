861bed731437a0d961f99222ee30bfc87e05435a
vishwakftw <cs15btech11043@iith.ac.in>
2017-09-10 07:23:51 +0530

    PA-1 Update 18

--- Programming-Assignment-1/Question3/decision_tree.py
+++ Programming-Assignment-1/Question3/decision_tree.py
@@ -6,50 +6,180 @@ class DecisionTree:
 		"""
 			Constructor for the class
 		"""
+		import numpy
+		self.np		= numpy
 		self.root	= self.node()
 		
-	def node(self, right=None, left=None, val=None):
+	def node(self, split=None, val=None):
 		"""
 			Typical node for a tree
 			Args:
-				right		= Right Node. Defaults to None
-				left		= Left Node. Defaults to None
+				split		= List of splits (Generalize for n-way split)
 				val		= Value of the Node. Defaults to None
 			Returns:
 				Node dictionary
 		"""
-		return {'right': right, 'left': left, 'val': val}
-	
-	def insert(self, some_node, x):
+		return {'split': split, 'val': val}
+				
+	def fit(self, X, y, impurity_func='entropy'):
 		"""
-			Function to insert a node into a tree
+			Function called to generate a univariate decision tree out of a given dataset
 			Args:
-				x		= Value to insert
-				some_node	= Root of subtree to insert into
-			Returns:
-				Modified tree
-			"""
-		if some_node['val'] is None:
-			some_node['val'] = x
-		else:
-			if x > some_node['val']:
-				if some_node['right'] is None:
-					some_node['right'] = self.node(val=x)
-				else:
-					some_node['right'] = self.insert(some_node['right'], x)
-			else:
-				if some_node['left'] is None:
-					some_node['left'] = self.node(val=x)
-				else:
-					some_node['left'] = self.insert(some_node['left'], x)
-		return some_node
+				X		= 2D-list of shape [n_points, n_features]
+				Y		= 1D-list of shape [n_points]
+				impurity_func	= Impurity function. Defaults to 'entropy'. Other option is 'gini'.
+		"""
+		assert impurity_func in ['entropy', 'gini'], "Invalid Impurity Functions."
+		self.X			= X
+		self.y			= y
+		self.impurity_func	= impurity_func
 		
-	def display(self, some_node):
+		self.features_count	= len(X[0])
+		self.features_set	= []
+		for i in xrange(0, self.features_count):
+			self.features_set.append(list(set(self.np.array(X)[:,i].tolist())))
+			
+	def impurity_after_split(self, splits):
 		"""
-			Returns pre-order traversal display
+			Function to calculate the impurity after a split.
+			Args:
+				splits		= 3D-list of shape [n_splits, n_points_in_each_class, 2]
+			Returns:
+				Scalar value: impurity after split
 		"""
-		print some_node['val']
-		if some_node['left'] is not None:
-			self.display(some_node['left'])
-		if some_node['right'] is not None:
-			self.display(some_node['right'])
+		total_incoming	= 0
+		for s in splits:
+			for cl in s:
+				total_incoming	+= len(cl)
+
+		imp	= 0.0
+		for s in splits:
+			inc_this_split	= float(len(s[0]) + len(s[1]))
+			if inc_this_split == 0:
+				continue
+			class_probs	= [len(s[0])/inc_this_split, len(s[1])/inc_this_split]
+			imp		= imp + inc_this_split*self.impurity(class_probs)/total_incoming
+				
+		return imp
+		
+	def impurity(self, class_probs):
+		"""
+			Function to calculate the impurity of a node
+			Args:
+				class_probs	= Class Probability
+			Returns:
+				The impurity of a node
+		"""
+		assert sum(class_probs) == 1, "Class probabilities don't add up to 1"
+
+		val	= 0.0	
+		if self.impurity_func == 'entropy':
+			for cp in class_probs:
+				if round(cp, 5) == 0.0:
+					val	-= 0
+				else:
+					val	-= self.np.log(cp)*cp
+
+		elif self.impurity_func == 'gini':
+			for cp in class_probs:
+				val	+= cp*(1 - cp)
+			
+		return val
+		
+	def make_split(self, feature_no, value=None):
+		"""
+			Function to make a split
+			Args:
+				feature_no	= the index of the feature
+				value		= If the feature is a real value, then this will facilitate the 2-way split (< or otherwise)
+			Returns:
+				3D-list of shape [n_splits, n_points_in_each_class, 2] representing the indexes of points per class
+		"""
+		if value is None:
+			splits = []
+			for i in xrange(0, len(self.features_set[feature_no])):
+				splits.append([[], []])
+
+			for i in xrange(0, len(self.X)):
+				for j in xrange(0, len(self.features_set[feature_no])):
+					if self.features_set[feature_no][j] == self.X[i][feature_no]:
+						splits[j][self.y[i]].append(i)
+						
+		else:
+			splits	= [[[],[]], [[],[]]]
+			for i in xrange(0, len(self.X)):
+				if self.X[i][feature_no] < value:
+					splits[0][self.y[i]].append(i)
+				else:
+					splits[1][self.y[i]].append(i)
+					
+		return splits
+		
+	def get_best_split(self):
+		"""
+			Function to get the best split at a given node based on the some splitting techniques
+			Args:
+				No args
+			Returns:
+				Best achieved impurity after split, Best split, and if the best split was achieved on a
+				continuous attribute, then that splitting value
+		"""
+		best_imp_after_split	= 1.1
+		best_split		= None
+		best_split_val		= None
+		best_split_feature_no	= -1
+		for f_no in xrange(0, self.features_count):
+
+			# Split generated based on integer values taken
+			# Typically the continuous fields fall in this category
+			if type(self.X[0][f_no]).__name__ == 'int':
+
+				# Splitting based on the distinct values of the attributes
+				for f_val in self.features_set[f_no]:					
+					cur_split	= self.make_split(f_no, f_val)
+					cur_imp_split	= self.impurity_after_split(cur_split)
+					
+					if best_imp_after_split > cur_imp_split:
+						best_split		= cur_split
+						best_imp_after_split	= cur_imp_split
+						best_split_val		= f_val
+						best_split_feature_no	= f_no
+						
+				# Splitting based on the pair-wise average of any two distinct values of the attributes
+				for i in xrange(0, len(self.features_set[f_no])):
+					for j in xrange(i, len(self.features_set[f_no])):
+						f_val		= (self.features_set[f_no][i] + self.features_set[f_no][j])/2.0
+						cur_split	= self.make_split(f_no, f_val)
+						cur_imp_split	= self.impurity_after_split(cur_split)
+						
+						if best_imp_after_split > cur_imp_split:
+							best_split		= cur_split
+							best_imp_after_split	= cur_imp_split
+							best_split_val		= f_val
+							best_split_feature_no	= f_no
+							
+				# Splitting based on total average of all distinct values of the attributes
+				f_val	= 0.0
+				f_val	= self.np.mean(self.features_set[f_no])
+				cur_split	= self.make_split(f_no, f_val)
+				cur_imp_split	= self.impurity_after_split(cur_split)
+				
+				if best_imp_after_split > cur_imp_split:
+					best_split		= cur_split
+					best_imp_after_split	= cur_imp_split
+					best_split_val		= f_val	
+					best_split_feature_no	= f_no			
+
+			# Split generated based on string type attributes
+			# Typically categorical data falls in this category
+			elif type(self.X[0][f_no]).__name__ == 'str':
+				cur_split	= self.make_split(f_no)
+				cur_imp_split	= self.impurity_after_split(cur_split)
+				
+				if best_imp_after_split > cur_imp_split:
+					best_split		= cur_split
+					best_imp_after_split	= cur_imp_split
+					best_split_val		= None
+					best_split_feature_no	= f_no
+					
+		return best_imp_after_split, best_split, best_split_val, best_split_feature_no
