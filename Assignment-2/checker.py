ab1289fc0bf1566a4326b9d24e0f599d0196d49d
vishwakftw <cs15btech11043@iith.ac.in>
2017-10-26 14:39:59 +0530

    Assignment 2 Update 3

--- Assignment-2/checker.py
+++ Assignment-2/checker.py
@@ -3,15 +3,27 @@ def act(layer):
     return [1 if l >= 0 else 0 for l in layer]
 
 # Part A
+w1, w2, b1 = -0.5, -0.5, 0
+w3, w4, b2 = -1.5, 0.5, -0.5
+w5, w6, b3 = 0.5, -1.5, -0.5
+w7, w8, b4 = 0.5, 0.5, -1
+
 def inp_to_hid(x1, x2):
-    h1 = -x1 + x2 - 1
-    h2 = -x1 + x2
-    h3 = x1 - x2
-    h4 = x1 - x2 - 1
+    h1 = w1*x1 + w2*x2 + b1
+    h2 = w3*x1 + w4*x2 + b2
+    h3 = w5*x1 + w6*x2 + b3
+    h4 = w7*x1 + w8*x2 + b4
     return [h1, h2, h3, h4]
 
+w_1, w_2, w_3, w_4, b_1 = 0, 1, 1, 0, -1
+
 def hid_to_out(h1, h2, h3, h4):
-    return [h1 + h4 - 1]
+    return [w_1*h1 + w_2*h2 + w_3*h3 + w_4*h4 + b_1]
+
+print(act(inp_to_hid(0, 0)))                        # Should print [1, 0, 0, 0] h1 for (0, 0)
+print(act(inp_to_hid(0, 1)))                        # Should print [0, 1, 0, 0] h2 for (0, 1)
+print(act(inp_to_hid(1, 0)))                        # Should print [0, 0, 1, 0] h3 for (1, 0)
+print(act(inp_to_hid(1, 1)))                        # Should print [0, 0, 0, 1] h4 for (1, 1)
 
 print(act(hid_to_out(*act(inp_to_hid(0, 0)))))      # Should print 0
 print(act(hid_to_out(*act(inp_to_hid(0, 1)))))      # Should print 1
@@ -19,13 +31,17 @@ print(act(hid_to_out(*act(inp_to_hid(1, 0)))))      # Should print 1
 print(act(hid_to_out(*act(inp_to_hid(1, 1)))))      # Should print 0
 
 # Part B
+w1, w2, b1 = 1, -1, -1
+w3, w4, b2 = -1, 1, -1
 def inp_to_hid(x1, x2):
-    h1 = x1 - x2 - 1
-    h2 = -x1 + x2 - 1
+    h1 = w1*x1 + w2*x2 + b1
+    h2 = w3*x1 + w4*x2 + b2
     return [h1, h2]
 
+w_1, w_2, b_1 = 1, 1, -1
+
 def hid_to_out(h1, h2):
-    return [h1 + h2 - 1]
+    return [w_1*h1 + w_2*h2 + b_1]
 
 print(act(hid_to_out(*act(inp_to_hid(0, 0)))))      # Should print 0
 print(act(hid_to_out(*act(inp_to_hid(0, 1)))))      # Should print 1
--- README.md
+++ README.md
@@ -13,3 +13,10 @@ Coursework pertaining to CS6510 : Applied Machine Learning offered in Fall 2017
 + Naive-Bayes Classifier on a real world dataset with different metrics
 + Decision Trees on a real world dataset
 + Support Vector Machines with different Kernels on a real world dataset
+
+### Assignment 2 :
+
++ Classification (Multi-Class)
++ Clustering
++ Neural Networks
++ Regression Methods
